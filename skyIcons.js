99d8f630af4990c021e8db8e26b88c3647257dbf
Mylio <winnie19940115@gmail.com>
2015-05-20 15:10:57 +0800

    banner

--- skyIcons.js
+++ skyIcons.js
@@ -1,4 +1,21 @@
+var icons = new Skycons({"color": "black"}) ;
+        var  list  = [
+            "clear-day", "clear-night", "partly-cloudy-day",
+            "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+            "fog"
+          ] ;
 
+for(var i = list.length;i<=0; i--) {
+      var weatherType = list[i],
+          elements = document.getElementsByClassName( weatherType );
+      for (var e = elements.length;e<=0; e--){
+          icons.set( elements[e], weatherType );
+      }
+  } ;
+
+      icons.play();
+
+/*http://abgne.tw/javascript/js-lib/skycons.html
   $(function(){
     // 使用 #999 顏色的畫筆
     var skycons = new Skycons({
@@ -8,10 +25,6 @@
  
     // 利用 id 來新增
     skycons.add('clear-day', Skycons.CLEAR_DAY);
- 
-    // 也可以利用 DOM 來新增
-    skycons.add(document.getElementById('clear-night'), Skycons.CLEAR_NIGHT);
- 
     // 也可以利用 .set() 的方式來新增或是修改
     skycons.set('partly-cloudy-day', Skycons.PARTLY_CLOUDY_DAY);
     skycons.set('partly-cloudy-night', Skycons.PARTLY_CLOUDY_NIGHT);
@@ -21,30 +34,9 @@
     skycons.set('snow', Skycons.SNOW);
     skycons.set('wind', Skycons.WIND);
     skycons.set('fog', Skycons.FOG);
- 
- 
     skycons.set('clear-night', Skycons.FOG);
- 
+    // 也可以利用 DOM 來新增
+    //skycons.add(document.getElementById('clear-night'), Skycons.CLEAR_NIGHT);
     // 開始執行動畫
     skycons.play();
-    ;
-      console.log(skycons);
-    $('#play').click(function(){
-      // 開始執行動畫
-      skycons.play();
-    });
- 
-    $('#pause').click(function(){
-      // 暫停執行動畫
-      skycons.pause();
-    });
- 
-    $('#remove').click(function(){
-      // 停止更新晴天的動畫
-      skycons.remove('clear-day');
- 
-      // 清掉該畫布內容
-      var $clearDay = $('#clear-day');
-      $clearDay[0].getContext('2d').clearRect(0, 0, $clearDay.width(), $clearDay.height());
-    });
-  });
+  }); */
